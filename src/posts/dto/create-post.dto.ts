b97978ecd280f49af501773efcd44efecd11c92b
Hugo <hugolacerdar@gmail.com>
2022-01-31 12:01:28 -0300

    chore: add create post dto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,3 +1,13 @@
+import { IsEmail, IsString } from 'class-validator';
 import { Post } from '../entities/post.entity';
 
-export class CreatePostDto extends Post {}
+export class CreatePostDto extends Post {
+  @IsString()
+  title: string;
+
+  @IsString()
+  content: string;
+
+  @IsEmail()
+  authorEmail: string;
+}
