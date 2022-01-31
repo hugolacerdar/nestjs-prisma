aca7e21bf3614dd76d803029074d941632433d57
Hugo <hugolacerdar@gmail.com>
2022-01-31 12:01:59 -0300

    chore: add post entity

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,9 @@
-export class Post {}
+import { Prisma } from '@prisma/client';
+
+export class Post implements Prisma.PostUncheckedCreateInput {
+  id?: string;
+  title: string;
+  content: string;
+  published?: boolean;
+  authorId: string;
+}
